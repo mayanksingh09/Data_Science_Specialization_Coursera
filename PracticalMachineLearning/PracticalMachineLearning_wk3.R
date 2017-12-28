62b56db9d5071b584531e49a22ec3147e449bea1
Mayank <mayank@Mayanks-MacBook-Air.local>
2017-12-29 00:52:05 +0530

    Change in wk3
    Wk3 code final dec 28

--- PracticalMachineLearning/PracticalMachineLearning_wk3.R
+++ PracticalMachineLearning/PracticalMachineLearning_wk3.R
@@ -101,3 +101,29 @@ qplot(predict(modFit, testing), wage, data = testing)
 
 
 # MODEL BASED PREDICTION
+
+data(iris)
+library(ggplot2)
+names(iris)
+library(caret)
+
+table(iris$Species)
+
+inTrain <- createDataPartition(y = iris$Species, p = 0.7, list = FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+
+dim(training)
+dim(testing)
+
+modlda <- train(Species ~., data = training, method = "lda")
+modnb <- train(Species ~., data = training, method = "nb")
+
+plda <- predict(modlda, testing)
+pnb <- predict(modnb, testing)
+
+table(plda, pnb)
+
+# compare results
+equalPredictions <- (plda==pnb)
+qplot(Petal.Width, Sepal.Width, colour = equalPredictions, data = testing)
